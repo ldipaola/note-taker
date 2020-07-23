1fd1424bc3d2c454f929274b54c665bb18a8b0cb
Lukey <ldipaola66@gmail.com>
2020-07-23 20:09:45 +1000

    setup server.js with basic routes

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+    
+  });
+
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+
+
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
